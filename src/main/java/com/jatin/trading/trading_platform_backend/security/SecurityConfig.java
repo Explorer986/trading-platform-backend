30aac42ec788d48bb5df3ae1e5565b172612dc26
t-jatinhltpls <125971908+t-jatinhltpls@users.noreply.github.com>
2026-01-09 19:28:40 +0530

    added frontend url to Allow-Origin

--- src/main/java/com/jatin/trading/trading_platform_backend/security/SecurityConfig.java
+++ src/main/java/com/jatin/trading/trading_platform_backend/security/SecurityConfig.java
@@ -63,7 +63,7 @@ public class SecurityConfig {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOrigins(List.of(
                 "http://localhost:5173",
-                "https://YOUR_FRONTEND.vercel.app"
+                "https://trading-platform-frontend-hazel.vercel.app"
         ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
